89a718bf47b15d0b2813c89ed8109c0b3c34c292
Jack Peña <64166010+jpena925@users.noreply.github.com>
2022-05-05 08:14:19 -0700

    Time: 105 ms (27.79%), Space: 42.1 MB (73.63%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (needle.length === 0 || needle === haystack) return 0;
+
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    if (needle === haystack.substring(i, i + needle.length)) {
+      return i;
+    }
+  }
+
+  return -1;
+};
