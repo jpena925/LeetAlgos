cf9641c6caba7b78def846b885fc7976828b6ac4
Jack Peña <64166010+jpena925@users.noreply.github.com>
2022-06-16 08:27:53 -0700

    Time: 126 ms (9.67%), Space: 42.9 MB (61.63%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,15 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let res = []
-    for(let i = 0; i < numbers.length; i++){
-        let compliment = target - numbers[i]
-        for(let j = i + 1; j < numbers.length; j++){
-            if(numbers[j] > compliment) break
-            if(numbers[j] === compliment){
-                res = [i+1, j+1]
-                return res
-            }
+    let start = 0
+    let end = numbers.length - 1
+    let result = []
+    
+    while (start < end) {
+        const sum = numbers[start] + numbers[end]
+        if (sum === target) {
+            return [start + 1, end + 1]
+        } else if (sum < target) {
+            start++
+        } else if (sum > target) {
+            end --
         }
     }
+    
+    return result
 };
