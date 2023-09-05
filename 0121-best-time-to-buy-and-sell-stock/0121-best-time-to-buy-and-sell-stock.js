055a5bda0d9a121dcbb1f5b251a22e9468b6f0a8
Jack Peña <64166010+jpena925@users.noreply.github.com>
2023-09-05 15:37:46 -0500

    Time: 67 ms (85.28%), Space: 51.9 MB (19.80%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let globalProfit = 0;
+  let minBuyPrice = Infinity;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (minBuyPrice > prices[i]) {
+      minBuyPrice = prices[i];
+    }
+
+    const currentProfit = prices[i] - minBuyPrice;
+
+    if (currentProfit > globalProfit) {
+      globalProfit = currentProfit;
+    }
+  }
+
+  return globalProfit;
+}
